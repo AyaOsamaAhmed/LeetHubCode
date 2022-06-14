49bcc2199bc7c9d685271f7687b51f5fea29b33e
Aya Osama Ahmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2022-06-15 01:24:14 +0200

    Time: 295 ms (73.08%), Space: 41.6 MB (46.15%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun minDistance(word1: String, word2: String): Int {
+    
+       val dp = Array(word1.length + 1) { IntArray(word2.length + 1) }
+        
+        for (i in 0..word1.length) {
+            for (j in 0..word2.length) {
+                dp[i][j] = when {
+                    i == 0 -> j
+                    j == 0 -> i
+                    word1[i-1] == word2[j-1] -> dp[i-1][j-1]
+                    else -> minOf(dp[i-1][j], dp[i][j-1]) + 1
+                }
+                
+            }
+        }
+        
+        return dp[word1.length][word2.length]
+    }
+    
+}
