f74e9d720031574f07f0c5d0559c9126b66c23db
Aya Osama Ahmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2022-06-17 01:21:06 +0200

    Time: 596 ms (43.81%), Space: 37.2 MB (79.89%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun longestPalindrome(s: String): String {
+         if (s.length == 1) return s
+    
+    for (maxIndex in s.length - 1 downTo 0) {
+      for (push in 0 until s.length - maxIndex) {
+        var i = push
+        var j = maxIndex + push
+        var found = true
+        
+        while (i < j) {
+          if (s[i++] == s[j--]) continue
+          
+          found = false
+          break
+        }
+        
+        if (found) return s.substring(push, maxIndex + push + 1)
+      }
+    }
+    
+    return ""
+        
+    }
+}
