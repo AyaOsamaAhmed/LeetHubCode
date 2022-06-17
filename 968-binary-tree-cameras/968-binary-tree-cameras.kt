59c5a7004247a1252b34c293efa52fcc94c8a9ee
Aya Osama Ahmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2022-06-18 00:26:16 +0200

    Time: 219 ms (100.00%), Space: 40.7 MB (11.11%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.kt
@@ -0,0 +1,36 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun minCameraCover(root: TreeNode?): Int {
+         val (NOT_MONITERED, HAS_CAMERA, MONITERED) = listOf(0, 1, 2)
+        var total = 0
+        
+        fun postOrder(node: TreeNode?): Int {
+            if (node == null) return MONITERED
+            val left = postOrder(node.left)
+            val right = postOrder(node.right)
+            if (left == NOT_MONITERED || right == NOT_MONITERED) {
+                total++
+                return HAS_CAMERA
+            }
+            if (left == MONITERED && right == MONITERED) {
+                return NOT_MONITERED
+            } else {
+                return MONITERED
+            }
+        }
+        
+        if (postOrder(root) == NOT_MONITERED) {
+            total++
+        }
+        return total
+    }
+}
