305dd585310bd2a24a3c43f09d138f6adc9a2bb3
Aya Osama Ahmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2022-06-20 14:24:27 +0200

    Time: 141 ms (93.79%), Space: 33 MB (90.68%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.kt
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.kt
@@ -1,11 +1,11 @@
 class Solution {
     fun countOdds(low: Int, high: Int): Int {
-        var count = 0 
+       /* var count = 0 
         for (i in low .. high){
             if(i %2 != 0 )
                 count ++
-        }
+        }*/
         
-        return count
+        return  (high - low)/2 + if(high % 2 !=0 || low % 2 != 0 ) 1 else 0
     }
 }
